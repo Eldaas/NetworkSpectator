670f2fa16eb064e4802decd882c16ae999f605e0
Eldaas <danieljones91@gmail.com>
2021-04-28 12:38:15 +0800

    Initial commit

---
+++ Assets/NetworkPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkPlayer : NetworkBehaviour
+{
+    [SyncVar]
+    public float currentSpeed = 2;
+
+    private void Start()
+    {
+        if (!isLocalPlayer) 
+        {
+            transform.GetChild(0).gameObject.SetActive(false); 
+            RpcTellServer();
+        }
+    }
+
+    [ClientRpc]
+    public void RpcTellServer()
+    {
+        CmdTellClients($"{gameObject.name} has joined");
+    }
+
+    [Command]
+    public void CmdTellClients(string message)
+    {
+        Debug.Log(message);
+    }
+}
